dd380eec64c0b3a8fb5ba9e3fb2c24da149cc28b
sunpengsdu <sunpengsdu@gmail.com>
2016-08-08 22:27:39 +0800

    two methods

--- t.py
+++ t.py
@@ -1,191 +1,191 @@
 from pulp import *
+import random
+import time
 
-prob = LpProblem('lptest', LpMaximize)
+random.seed(1)
 
-#lamda = 0.05
-lamda = 0.0
-relaxed_fairness = 0.2
-num_app = 3
-#'''
-cpu_app = [1, 4, 1]
-ram_app = [4, 2, 2]
-weight  = [1, 1, 1]
-domain  = ['cpu', 'cpu', 'cpu']
-'''
+server_num   = 200;
+job_num      = 100;
+resource_num = 3;
+servers      = range(server_num);
+jobs         = range(job_num);
+resources    = range(resource_num);
+known_job_sw     = {};
+unknown_job_sw   = {};
+history_job_info = [];
+resources_total  = {};
+job_demand       = {}; #[job][resource]
+resouce_capacity = {}; #[server][resource]
+job_max_worker   = {};
+job_min_worker   = {};
+job_weight       = {};
+iteration_num    = 0;
 
-#cpu_app = [1, 3]
-#ram_app = [3, 4]
-num_app = 2
-cpu_app = [1, 4]
-ram_app = [4, 2]
-weight  = [1, 1]
-domain  = ['cpu', 'cpu', 'cpu']
-'''
-num_server = 2
-#cpu_server = [9, 9]
-#ram_server = [18, 18]
-cpu_server = [12, 24]
-ram_server = [128, 128]
+#define job resource demand
+for i in jobs:
+    job_demand[i] = {};
+    for j in resources:
+        if j==0:
+            job_demand[i][j] = random.randint(2,8);  #CPU
+        elif j==1:
+            job_demand[i][j] = random.randint(8,16); #RAM
+        elif j==2:
+            job_demand[i][j] = random.randint(1,1);  #GPU
 
-old_alloc = [[12,12], [0,3], [0,0]] #[j][i], j-th app, i-th server
+#define resource capacity
+for i in servers:
+    resouce_capacity[i] = {};
+    for j in resources:
+        if j==0:
+            resouce_capacity[i][j] = 24;
+        elif j==1:
+            resouce_capacity[i][j] = 200;
+        elif j==2:
+            resouce_capacity[i][j] = 4;
 
-existing_app = []
-for i in range(num_app):
-    if sum(old_alloc[i]) > 0:
-        existing_app.append(i)
+for i in resources:
+    resources_total[i] = sum(resouce_capacity[m][i] for m in servers);
 
-global_cpu_num = sum(cpu_server)
-global_ram_num = sum(ram_server)
-global_cpu_ram_ratio = global_cpu_num * 1.0 / global_ram_num
+#define max/min worker number and weight
+for i in jobs:
+    job_max_worker[i] = random.randint(10,50);
+    job_min_worker[i] = random.randint(0,0);
+    job_weight[i]     = random.randint(1,10);
+    unknown_job_sw[i] = 0;
 
-app_cpu_ram_ratio = []
-for i in range(num_app):
-    app_cpu_ram_ratio.append(cpu_app[i] * 1.0 / ram_app[i])
+##############################
+# assignment = [];  #[job][server]
+# for i in jobs:
+#     assignment.append([]);
+#     for j in servers:
+#         assignment[i].append(0);
+# 
+# utilization = []; #[server][resource]
+# for i in servers:
+#     utilization.append([]);
+#     for j in resources:
+#         utilization[i].append(0);
+#
+# job_sw = {}; #[job]
+# for i in jobs:
+#     job_sw[i] = 0;
+#
+# is_full = False;
+# while is_full == False:
+#     tmp_sw_value = job_sw.values();
+#     min_sw_value = min(tmp_sw_value);
+#     min_sw_index = job_sw.values().index(min_sw_value);
+#     min_sw_key   = job_sw.keys()[min_sw_index];
+#     dominant_resource_share = max([1.0*job_demand[min_sw_key][n]/resources_total[n] for n in resources]);
+#     dominant_resource       = [1.0*job_demand[min_sw_key][n]/resources_total[n] for n in resources].index(dominant_resource_share);
+#     dominant_resource_util = [utilization[m][dominant_resource]*1.0/resouce_capacity[m][dominant_resource] for m in servers];
+#     sorted_resource_util = sorted(dominant_resource_util);
+#     sorted_server = [];
+#     for i in sorted_resource_util:
+#         sorted_server.append(dominant_resource_util.index(i));
+#
+#     for target_server in sorted_server:
+#         chosen = True;
+#         for try_resource in resources:
+#             if utilization[target_server][try_resource]+job_demand[min_sw_key][try_resource]\
+#                 > resouce_capacity[target_server][try_resource]:
+#                 chosen = False;
+#                 break;
+#         if chosen == True:
+#             assignment[min_sw_key][target_server] = assignment[min_sw_key][target_server] + 1;
+#             for try_resource in resources:
+#                 utilization[target_server][try_resource] = utilization[target_server][try_resource]+job_demand[min_sw_key][try_resource];
+#             job_sw[min_sw_key] = sum([assignment[min_sw_key][n] for n in servers]) \
+#                 * max([1.0*job_demand[min_sw_key][n]/resources_total[n] for n in resources]) \
+#                / job_weight[min_sw_key];
+#             if sum([assignment[min_sw_key][n] for n in servers]) == job_max_worker[min_sw_key]:
+#                 del job_sw[min_sw_key];
+#             break;
+#     if chosen == False:
+#         is_full = True;
+#
+# for i in jobs:
+#     print sum([assignment[i][n] for n in servers]),
+#################################
 
-'''
-for i in range(num_app):
-    if app_cpu_ram_ratio[i] >= global_cpu_ram_ratio:
-        domain[i] = 'cpu'
-    else:
-        domain[i] = 'ram'
-'''
+start_time = time.time();
+while len(unknown_job_sw) > 0:
+    iteration_num = iteration_num + 1;
+    print '######->', iteration_num;
+    prob = LpProblem('cacluate_fair', LpMaximize)
+    #define main variables
+    assignment = LpVariable.dicts(name="assignment", indexs=(jobs, servers), lowBound=0, cat=LpInteger);
 
-#domain  = ['ram', 'cpu']
+    #Constraint: Resource Capacity
+    for i in servers:
+        for j in resources:
+            prob += lpSum([job_demand[m][j]*assignment[m][i] for m in jobs]) <= resouce_capacity[i][j];
 
-print "application domain:"
-for i in range(num_app):
-    print domain[i], '\t',
-print ""
-print "global cpu ram ratio: ", global_cpu_ram_ratio
-print ""
+    #Constrain: max worker number
+    for i in jobs:
+        prob += lpSum([assignment[i][n] for n in servers]) <= job_max_worker[i];
 
-variables = []
-single_variables = []
-regu = []
-single_regu = []
+    #Constrain: min worker number
+    for i in jobs:
+        prob += lpSum([assignment[i][n] for n in servers]) >= job_min_worker[i];
 
-weight_sum = sum(weight)
-for i in range(num_app):
-    weight[i] = weight[i]*1.0/weight_sum
+    for i in known_job_sw.keys():
+        prob += (lpSum([assignment[i][n] for n in servers]) \
+                * max([1.0*job_demand[i][n]/resources_total[n] for n in resources]) \
+                / job_weight[i] - known_job_sw[i]) / known_job_sw[i] \
+                <= 0.1;
+        prob += (-lpSum([assignment[i][n] for n in servers]) \
+                * max([1.0*job_demand[i][n]/resources_total[n] for n in resources]) \
+                / job_weight[i] + known_job_sw[i]) / known_job_sw[i] \
+                <= 0.1;
 
-for i in range(num_server):
-    variables.append([])
-    regu.append([])
-    for j in range(num_app):
-        t = LpVariable('app-'+str(i)+'-'+str(j), lowBound=0, cat=LpInteger)
-        variables[i].append(t)
-        single_variables.append(t)
-        tmp_regu = LpVariable('regu-'+str(i)+'-'+str(j))
-        regu[i].append(tmp_regu)
-        if i in existing_app:
-            single_regu.append(tmp_regu)
+    tau = LpVariable(name='tau', cat='Continuous');
 
-for i in range(num_server):
-    for j in range(num_app):
-        #   prob += max(-variables[i][j] + old_alloc[j][i], 0)
-        prob += regu[i][j] >= -variables[i][j] + old_alloc[j][i]
-        prob += regu[i][j] >= 0
+    for i in unknown_job_sw.keys():
+        prob += lpSum([assignment[i][n] for n in servers]) \
+                * max([1.0*job_demand[i][n]/resources_total[n] for n in resources]) \
+                / job_weight[i] >= tau;
 
-#CPU
-for i in range(num_server):
-    prob += sum([cpu_app[j]*variables[i][j]  for j in range(num_app)]) <= cpu_server[i]
+    for i in known_job_sw.keys():
+        prob += lpSum([assignment[i][n] for n in servers]) \
+                 * max([1.0*job_demand[i][n]/resources_total[n] for n in resources]) \
+                 / job_weight[i] <= tau/0.9;
 
-for i in range(num_server):
-    prob += sum([ram_app[j]*variables[i][j]  for j in range(num_app)]) <= ram_server[i]
+    prob += tau;
+    solver = solvers.CPLEX_PY();
+    solver.epgap = 0.1;
+    # prob.solve(CPLEX());
+    solver.solve(prob);
+    # prob.solve(GLPK(options=['--mipgap', '0.02']));
 
+    result = [];
+    for i in unknown_job_sw.keys():
+        result.append(sum([value(assignment[i][n]) for n in servers]) \
+        * max([1.0*job_demand[i][n]/resources_total[n] for n in resources]) \
+        / job_weight[i]);
+    min_result = min(result);
+    min_result_index = unknown_job_sw.keys()[result.index(min_result)];
+    known_job_sw[min_result_index] = min_result;
+    del unknown_job_sw[min_result_index];
 
-cpu_total = sum(cpu_server)
-ram_total = sum(ram_server)
+    print min_result, '---', value(tau);
+    job_info = []
+    for i in jobs:
+        tmp = int(sum([value(assignment[i][n]) for n in servers]));
+        job_info.append(tmp);
+    history_job_info.append(job_info);
 
-cpu_app_used = []
-for i in range(num_server):
-    for j in range(num_app):
-        if j < len(cpu_app_used):
-            cpu_app_used[j].append(cpu_app[j]*variables[i][j])
-        else:
-            cpu_app_used.append([cpu_app[j]*variables[i][j]])
+    break_tag = True;
+    for i in unknown_job_sw.keys():
+        if job_info[i] == job_max_worker[i]:
+            break_tag = False;
+            break;
+    if job_info[min_result_index] == job_max_worker[min_result_index]:
+        break_tag = False;
 
-ram_app_used = []
-for i in range(num_server):
-    for j in range(num_app):
-        if j < len(ram_app_used):
-            ram_app_used[j].append(ram_app[j]*variables[i][j])
-        else:
-            ram_app_used.append([ram_app[j]*variables[i][j]])
+    if break_tag == True:
+        break;
 
-domain_share = []
-cpu_app_share = {}
-ram_app_share = {}
-
-cpu_app_share2 = []
-ram_app_share2 = []
-for i in range(num_app):
-    cpu_app_share[i] = LpVariable('cpu_app_share_'+str(i), lowBound = 0)
-    ram_app_share[i] = LpVariable('ram_app_share_'+str(i), lowBound = 0)
-    prob += cpu_app_share[i] ==  sum(cpu_app_used[i]) *1.0 / cpu_total
-    prob += ram_app_share[i] ==  sum(ram_app_used[i]) *1.0/ ram_total
-    cpu_app_share2.append(cpu_app_share[i])
-    ram_app_share2.append(ram_app_share[i])
-
-    if domain[i] == 'cpu':
-        domain_share.append(cpu_app_share[i])
-    else:
-        domain_share.append(ram_app_share[i])
-
-prob += sum(cpu_app_share2) + sum(ram_app_share2)  - lamda * sum([j for j in single_regu])
-
-for i in range(num_app):
-   # prob  += domain_share[i] - sum(domain_share) * weight[i] == 0
-    prob  += domain_share[i] - sum(domain_share) * weight[i] >= -relaxed_fairness
-    prob  += domain_share[i] - sum(domain_share) * weight[i] <=  relaxed_fairness
-
-#x1 = LpVariable('x1', lowBound = 0, cat=LpInteger)
-#x2 = LpVariable('x2', lowBound = 0, cat=LpInteger)
-#prob += x1 + x2
-#prob += 4*x1 + x2 <= 130
-#prob += x1 + 3*x2 <= 89
-#prob += 6*variables[0][0] - 9*variables[0][1] == 0
-
-#prob.solve(GLPK(options=['--mipgap', '0.01']))
-#prob.solve(CPLEX(options=['--mipgap', '0.01']))
-#prob.solve(GLPK())
-prob.solve(CPLEX())
-#for v in prob.variables():
-#    print v.name, '=', v.varValue
-
-result = prob.variablesDict();
-
-total_app_num = [];
-
-print "\napp allocations:"
-for i in range(num_server):
-    print "Server ", i, ' ->\t',
-    for j in range(num_app):
-        print result['app_'+str(i)+'_'+str(j)].varValue, '\t',
-        if j >= len(total_app_num):
-            total_app_num.append(result['app_'+str(i)+'_'+str(j)].varValue)
-        else:
-            total_app_num[j] += result['app_'+str(i)+'_'+str(j)].varValue
-    print ''
-
-for i in range(num_app):
-    print 'app ', i, ' num -> ', total_app_num[i]
-
-print "\nregu:"
-for i in range(num_server):
-    print "Server ", i, ' ->\t',
-    for j in range(num_app):
-        print result['regu_'+str(i)+'_'+str(j)].varValue, '\t',
-    print ''
-
-print "\nCPU utilization: ",
-total_cpu = 0
-for i in range(num_app):
-    total_cpu += cpu_app[i] * total_app_num[i]
-print total_cpu * 1.0 / sum(cpu_server)
-
-print "\nRAM utilization: ",
-total_ram = 0
-for i in range(num_app):
-    total_ram += ram_app[i] * total_app_num[i]
-print total_ram * 1.0 / sum(ram_server)
+print iteration_num;
+print history_job_info;
+print("--- %s seconds ---" % (time.time() - start_time));
